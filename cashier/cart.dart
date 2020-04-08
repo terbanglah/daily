d0aaffa15de3b8f987ace292ebc99a2d7a3596a9
syamarifin <asyamsul95@gmail.com>
2020-04-08 14:54:21 +0700

    fixed Kasir

--- cashier/cart.dart
+++ cashier/cart.dart
@@ -152,6 +152,40 @@ class _CartPageState extends State<CartPage> {
           )
         ],
       ),
+      // appBar: PreferredSize(
+      //   preferredSize: Size(double.infinity, 100),
+      //   child: Container(
+      //     decoration: BoxDecoration(
+      //       boxShadow: [BoxShadow(
+      //         color: Colors.black12,
+      //         spreadRadius: 5,
+      //         blurRadius: 2
+      //       )]
+      //     ),
+      //     width: MediaQuery.of(context).size.width,
+      //     height: 90,
+      //     child: Container(
+      //       decoration: BoxDecoration(
+      //         color: Warnadasar.menuFood,
+      //         // borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20),bottomRight: Radius.circular(20))
+      //       ),
+      //       child: Container(
+      //         padding: EdgeInsets.only(top: 20.0,left: 10.0),
+      //         margin: EdgeInsets.fromLTRB(0,20, 0, 0),
+      //         child: Row(
+      //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //           crossAxisAlignment: CrossAxisAlignment.center,
+      //           children: [
+      //             Icon(Icons.arrow_back,size: 28,color: Colors.white,),
+      //             // Image.asset('logo1.png'),
+      //             Text("Kasir",style: TextStyle(fontSize: 20,color: Warnadasar.white, fontWeight: FontWeight.bold),),
+      //             Icon(Icons.navigate_before,color: Colors.transparent,),
+      //           ],
+      //         ),
+      //       ),
+      //     ),
+      //   ),
+      // ),
       body: RefreshIndicator(
         key: _refresh,
         onRefresh: _readData,
